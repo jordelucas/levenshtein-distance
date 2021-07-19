e68bd1350840d1645e7824832d85b6bdfba1e8f4
Jordevá <jordevalucas@gmail.com>
2021-07-19 17:36:07 -0300

    implementação concorrente

--- src/ufrn/imd/dim0124/LevenshteinDistance.java
+++ src/ufrn/imd/dim0124/LevenshteinDistance.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 
 public class LevenshteinDistance {
 
-	static int calculate(String x, String y) {
+	public int calculate(String x, String y) {
 		int[][] dp = new int[x.length() + 1][y.length() + 1];
 
 	    for (int i = 0; i <= x.length(); i++) {
@@ -26,11 +26,11 @@ public class LevenshteinDistance {
 	    return dp[x.length()][y.length()];
     }
 
-    public static int costOfSubstitution(char a, char b) {
+    private static int costOfSubstitution(char a, char b) {
         return a == b ? 0 : 1;
     }
 
-    public static int min(int... numbers) {
+    private static int min(int... numbers) {
         return Arrays.stream(numbers)
           .min().orElse(Integer.MAX_VALUE);
     }
--- src/ufrn/imd/dim0124/Main.java
+++ src/ufrn/imd/dim0124/Main.java
@@ -3,22 +3,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.Iterator;
+import java.util.ArrayList;
 import java.util.LinkedHashSet;
+import java.util.List;
 
 public class Main {
+	public static int THREAD_SIZE = 1;
 	
-	public static final int DISTANCE_DEFAULT = 100;
-
 	public static void main(String[] args) throws IOException {
-		int count = 0;
-
-		int position_best_distance = 0;
-		int value_best_distance = DISTANCE_DEFAULT;
-		String username_best_distance = "";
-
-		String username_to_search = "catarina";
-		
 		LinkedHashSet<String> lhs = new LinkedHashSet<String>();
 
 		InputStream res = Main.class.getResourceAsStream("/animelists.csv");
@@ -29,22 +21,10 @@ public class Main {
 			}
 		}
 		
-		Iterator<String> itr = lhs.iterator();
-        while (itr.hasNext()) {
-        	count++;
-    		String current_username = itr.next();
-    		int current_distance = LevenshteinDistance.calculate(username_to_search, current_username);
-    		if (current_distance < value_best_distance) {
-    			value_best_distance = current_distance;
-    			username_best_distance = current_username;
-    			position_best_distance = count;
-    			
-    			if(current_distance == 0) {
-    				break;
-    			}
-    		}
-        }
-
-		System.out.println(position_best_distance + ": " + username_best_distance + " | " + value_best_distance);
+		List<String> list = new ArrayList<String>(lhs);
+		
+		String result = new SearchWord().search(list, "thiffany", THREAD_SIZE);
+		
+		System.out.println(result);
 	}
 }
